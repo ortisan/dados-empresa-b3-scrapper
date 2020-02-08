5eb2c0a8632aa8f0af20dfcf67c1b758e9623abe
Logon Aluno <logonaluno@local.com>
2020-02-07 22:05:18 -0300

    Correção

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # Scrapper para extração dos dados das empresas listadas na B3.
 
 
+Extrai o nome e o link das informações das empresas listadas na B3.
+
+Execução:
+
+1. Na raiz do projeto executar:
+
 ```sh
+npm install
 node index.js
 ```
 
--- index.js
+++ index.js
@@ -7,14 +7,12 @@ function obter_dados_empresa(url) {
     .then(html => {
         var $ = cheerio.load(html);
         var tabela_empresas = $("#ctl00_contentPlaceHolderConteudo_BuscaNomeEmpresa1_grdEmpresa_ctl01");
-        let json_empresas = {}; 
+        let json_empresas = []; 
         $(".GridRow_SiteBmfBovespa", tabela_empresas).each(function (i, element) {
             console.log("Empresa:" + $(element).text().trim());
             console.log("Link: " + $('a', element).attr('href'));
-            let link_empresa = "http://bvmf.bmfbovespa.com.br/cias-listadas/empresas-listadas/" + $('a', element).attr('href');
-            let json_emp_x = [];
-            json_emp_x[$(element).text().trim()] = link_empresa 
-            json_empresas = {...json_emp_x}
+            let link_empresa = "http://bvmf.bmfbovespa.com.br/cias-listadas/empresas-listadas/" + $('a', element).attr('href');            
+            json_empresas[$(element).text().trim()] = link_empresa             
         });
         return json_empresas;
     })
@@ -24,10 +22,9 @@ function obter_dados_empresa(url) {
 }
 
 Promise.all(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Z'].map(letra => {
-    console.log(letra);
     const url = 'http://bvmf.bmfbovespa.com.br/cias-listadas/empresas-listadas/BuscaEmpresaListada.aspx?Letra='+letra+'&idioma=pt-br';
     return obter_dados_empresa(url);
-})).then(jsons_empresas => {
+})).then(jsons_empresas => {    
     return jsons_empresas.reduce((acum, json) => {
         return {...acum, ...json}
     });
